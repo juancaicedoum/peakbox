f88bdcf972b33b37aca15ab4a1c182cb2ec01312
Juan Manuel Caicedo <jmcaicedo69822@umanizales.edu.co>
2025-07-13 13:23:49 -0500

    Resolve merge conflict in config/boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,4 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
-<<<<<<< HEAD
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
-=======
->>>>>>> 6445efdc93586bdd4db684b09880f1298682850a
