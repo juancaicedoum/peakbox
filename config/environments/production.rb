3ce6eff35572720b92f10833d883a451a54408ac
Juan Manuel Caicedo <jmcaicedo69822@umanizales.edu.co>
2025-07-13 15:43:48 -0500

    Resuelto conflicto en config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+# config/environments/production.rb
+
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
@@ -6,95 +8,96 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
-  # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
+  # Eager load code on boot. This eager loads most of Rails and
+  # your application in order to prepare for production.
   config.eager_load = true
 
-  # Full error reports are disabled.
-  config.consider_all_requests_local = false
+  # Enable/disable caching. By default caching is disabled.
+  # Run rails dev:cache to toggle caching in development.
+  if Rails.root.join("tmp/caching-dev.txt").exist?
+    config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
 
-  # Turn on fragment caching in view templates.
-  config.action_controller.perform_caching = true
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
+    config.cache_store = :null_store
+  end
 
-  # Cache assets for far-future expiry since they are all digest stamped.
-  config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.asset_host = "http://assets.example.com"
+  # Specifies the header that your server uses for sending files.
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache and NGINX
+  # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
-<<<<<<< HEAD
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-=======
->>>>>>> 6445efdc93586bdd4db684b09880f1298682850a
-  # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  config.assume_ssl = true
+  # Mount Action Cable outside main process or loop that processes HTTP requests.
+  # config.action_cable.mount_path = nil
+  # config.action_cable.url = ENV["ACTION_CABLE_URL"]
+  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
 
-  # Skip http-to-https redirect for the default health check endpoint.
-  # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
+  # Use the lowest log level to ensure that all message types are logged.
+  # For more comprehensive logging, you may want to set this to :debug.
+  config.log_level = :info
 
-  # Log to STDOUT with the current request id as a default log tag.
+  # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
-  config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
 
-  # Change to "debug" to log everything (including potentially personally-identifiable information!)
-  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+  # Use a different cache store in production.
+  # config.cache_store = :mem_cache_store
 
-  # Prevent health checks from clogging up the logs.
-  config.silence_healthcheck_path = "/up"
+  # Use a real queuing backend for Active Job (and separate queues per environment).
+  # config.active_job.queue_adapter = :resque
+  # config.active_job.queue_adapter = :sidekiq
 
-  # Don't log any deprecations.
-  config.active_support.report_deprecations = false
-
-  # Replace the default in-process memory cache store with a durable alternative.
-<<<<<<< HEAD
-  config.cache_store = :solid_cache_store
-
-  # Replace the default in-process and non-durable queuing backend for Active Job.
-  config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
+  config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # Set this to true and configure your mailer to use a delivery method that does not raise.
   # config.action_mailer.raise_delivery_errors = false
 
-  # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
-
-  # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
-  # config.action_mailer.smtp_settings = {
-  #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
-  #   password: Rails.application.credentials.dig(:smtp, :password),
-  #   address: "smtp.example.com",
-  #   port: 587,
-  #   authentication: :plain
-  # }
-=======
-  # config.cache_store = :mem_cache_store
->>>>>>> 6445efdc93586bdd4db684b09880f1298682850a
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
+  # Enable locale fallbacks for I18n (makes lookups for original locale fall back to
+  # the development locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
-<<<<<<< HEAD
+  # Don't log any deprecations.
+  config.active_support.deprecation = :silence
+
+  # Log disallowed deprecations to Rails.logger by default.
+  # config.active_support.disallowed_deprecation_warnings = []
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # Only use :id for inspections in production.
-  config.active_record.attributes_for_inspect = [ :id ]
+  # Enable query cache by default.
+  config.active_record.query_cache_enabled = true
 
-=======
->>>>>>> 6445efdc93586bdd4db684b09880f1298682850a
-  # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
-  #
-  # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  # Inserts middleware to perform automatic browser detection on assets requests.
+  # config.assets.js_compressor = :uglifier
+
+  # Compress CSS using a CSS compressor or Sass.
+  # config.assets.css_compressor = :sass
+
+  # Use the local file system for storing cache.
+  # config.cache_store = :file_store, "tmp/cache/production"
+
+  # Enable serving of images, stylesheets, and JavaScript from an asset server.
+  # config.action_controller.asset_host = "http://assets.example.com"
+
+  # Enable JavaScript compilation with Webpacker if you're using it.
+  # config.webpacker.check_yarn_integrity = false
+
+  # You need to explicitly enable `config.action_view.button_to_uses_form_method_display` if you use it.
+  # This option will be removed in Rails 8.0.
+  # config.action_view.button_to_uses_form_method_display = true
+
+  # Uncomment if you're not using strict_loading mode in your production app.
+  # config.active_record.strict_loading_by_default = true
 end
