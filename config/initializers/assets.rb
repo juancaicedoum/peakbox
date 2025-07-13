6f8b160305eed5d88fca53f896377b30ddfea151
Juan Manuel Caicedo <jmcaicedo69822@umanizales.edu.co>
2025-07-13 13:33:17 -0500

    Resuelto conflicto en config/initializers/assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,11 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-<<<<<<< HEAD
 Rails.application.config.assets.version = "1.0"
-=======
-#Rails.application.config.assets.version = "1.0"
->>>>>>> 6445efdc93586bdd4db684b09880f1298682850a
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
