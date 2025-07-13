141ba3c17d194b179c7981c3b3cf5adf62ec08ce
Juan Manuel Caicedo <jmcaicedo69822@umanizales.edu.co>
2025-07-13 16:01:12 -0500

    Configure SECRET_KEY_BASE in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
-
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
   # Eager load code on boot. This eager loads most of Rails and
   # your application in order to prepare for production.
   config.eager_load = true
